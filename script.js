95c5168ef2a7d5a51a5f931af1c7a9ca95697da4
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-27 13:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-
+let formLogin  = document.querySelector(".loginForm")
 let user_box = document.querySelector("#username");
 let pass_box = document.querySelector("#password");
 let check_box = document.querySelector("#checkbox");
 let sub_box = document.querySelector("#submit");
 
-let eixt_btn = document.querySelector("#existing");
+let exit_btn = document.querySelector("#existing");
+
+exit_btn.style.display = "none";
+
+
+// let obj = {};
+
+formLogin.addEventListener("submit",handleSubmit);
+
+function handleSubmit(e) {
+	e.preventDefault();
+
+	let username = user_box.value;
+	let userpass = pass_box.value;
+
+	if(check_box.checked){
+		 localStorage.setItem("username", username);
+        localStorage.setItem("password", userpass);
+		exit_btn.style.display = "inline-block";
+		
+	}
+	alert(`Logged in as ${username}`);
+}
+
+
+
+
+
+
+
+
 
 
 
